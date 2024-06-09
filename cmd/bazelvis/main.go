5a377f3e488ca2e0b8bb45f68304e7dc70b8e2d1
James Ma <jamesma100@gmail.com>
2024-06-08 17:41:54 -0700

    fmt

--- cmd/bazelvis/main.go
+++ cmd/bazelvis/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/jamesma100/bazelvis/pkg/runner"
-	"github.com/jamesma100/bazelvis/pkg/parser"
-	"os"
+  "github.com/jamesma100/bazelvis/pkg/runner"
+  "github.com/jamesma100/bazelvis/pkg/parser"
+  "os"
   "fmt"
 )
 

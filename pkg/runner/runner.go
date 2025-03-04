805980e73ae5c4373da5ece9254fdc783262df95
James Ma <jamesma100@gmail.com>
2025-03-04 12:08:55 -0800

    don't truncate graph

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -4,7 +4,7 @@ import "os/exec"
 import "log"
 
 func GetGraph(arg string) string {
-	cmd := exec.Command("bazel", "query", "--notool_deps", "deps("+arg+")", "--output", "graph")
+	cmd := exec.Command("bazel", "query", "--notool_deps", "deps("+arg+")", "--output", "graph", "--graph:node_limit", "-1")
 	log.Printf("Running command: %s\n", cmd)
 	stdout, err := cmd.Output()
 	if err != nil {
